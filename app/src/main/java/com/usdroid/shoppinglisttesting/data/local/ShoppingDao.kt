ee35ff4c2d72e61495076e5f878e17e5ddfe775b
Osama <osama@visions-tech.net>
2023-01-30 00:14:24 +0200

    Initial commit

---
+++ app/src/main/java/com/usdroid/shoppinglisttesting/data/local/ShoppingDao.kt
@@ -0,0 +1,9 @@
+package com.usdroid.shoppinglisttesting.data.local
+
+import androidx.room.Dao
+
+@Dao
+interface ShoppingDao {
+
+
+}
---
+++ app/src/main/java/com/usdroid/shoppinglisttesting/data/local/ShoppingItem.kt
@@ -0,0 +1,14 @@
+package com.usdroid.shoppinglisttesting.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    var name: String,
+    var amount: Int,
+    var price: Float,
+    var imageUrl: String,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+)
